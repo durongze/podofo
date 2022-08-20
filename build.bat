c34ad420c2c696d7c6e3b9df40891a69e84c6d1b
durongze <durongze@qq.com>
2022-08-20 10:21:22 +0800

    fix helloworld

--- build.bat
+++ build.bat
@@ -1,16 +1,22 @@
 @rem %comspec% /k "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 @rem call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-@rem %comspec% /k "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+%comspec% /k "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
 call "E:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+@rem %comspec% /k "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+@rem call "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\Tools\VsDevCmd.bat"
+@rem %comspec% /k "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+@rem call "E:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 set PATH=%PATH%;E:\msys64\usr\bin;
 md dyzbuild
 pushd dyzbuild
-	@rem del * /q /s
+	del * /q /s
+	@rem cmake .. -G"Visual Studio 17 2022" -A Win64
 	@rem cmake .. -G"Visual Studio 16 2019" -A Win64
 	@rem cmake --build . --target clean
 	@rem cmake --build . -j16
 	cmake ..
-	cmake --build . --target examples\helloworld\helloworld
+	cmake --build . --target examples\helloworld\helloworld  -j16
+	@rem cmake --build .
 	dir .\examples\helloworld\helloworld.exe
 	@rem .\examples\helloworld\helloworld.exe
 popd
