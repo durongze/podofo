8b588948da7be1555aedfe7383df26eb91515647
durongze <durongze@qq.com>
2023-11-19 13:03:25 +0800

    build ok

--- build_custom.bat
+++ build_custom.bat
@@ -1,14 +1,5 @@
 @rem set VSCMD_DEBUG=2
 @rem %comspec% /k "F:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
-set PerlPath=F:\program\Perl\bin
-set NASMPath=F:\program\nasm
-set CMakePath=F:\program\cmake\bin
-set PATH=%NASMPath%;%PerlPath%;%CMakePath%;%PATH%
-
-set CurDir=%~dp0
-
-set ProjDir=%CurDir:~0,-1%
-echo ProjDir %ProjDir%
 
 set VisualStudioCmd="F:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
 
@@ -33,7 +24,21 @@ set old_sys_include="%include%"
 set old_sys_lib="%lib%"
 set old_sys_path="%path%"
 
-call :SetProjEnv %CurDir% include lib path
+set ProgramDir=F:\program
+set PerlPath=%ProgramDir%\Perl\bin
+set NASMPath=%ProgramDir%\nasm
+set CMakePath=%ProgramDir%\cmake\bin
+set PythonHome=%ProgramDir%\python
+set PATH=%NASMPath%;%PerlPath%;%CMakePath%;%PythonHome%;%PATH%
+
+set CurDir=%~dp0
+
+set ProjDir=%CurDir:~0,-1%
+echo ProjDir %ProjDir%
+set software_dir="%ProjDir%\thirdparty"
+set HomeDir=%ProjDir%\out\windows
+
+call :SetProjEnv %CurDir% include lib path CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_MODULE_PATH
 call :ShowProjEnv
 
 set SystemBinDir=.\
@@ -53,14 +58,17 @@ pause
 goto :eof
 
 :CompileProject
-    setlocal ENABLEDELAYEDEXPANSION
+    setlocal EnableDelayedExpansion
     set BuildDir="%~1"
     set BuildType="%~2"
     set ProjName=%~3
 
     if not exist %BuildDir% (
         mkdir %BuildDir%
+    ) else (
+        mkdir %BuildDir%\%BuildType%\
     )
+    for /f %%i in ('dir /s /b "out\windows\*.dll"') do (   copy %%i %BuildDir%\%BuildType%\ )
     pushd %BuildDir%
         @rem del * /q /s
         @rem cmake .. -G"Visual Studio 16 2019" -A Win64
@@ -121,13 +129,12 @@ goto :eof
     set loc_inc=%2
     set loc_lib=%3
     set loc_bin=%4
-    set auto_install_func=%software_dir%\auto_func.bat
 
-    call %auto_install_func% gen_all_env %software_dir% %HomeDir% loc_inc loc_lib loc_bin
+    call :gen_all_env %software_dir% %HomeDir% loc_inc loc_lib loc_bin cmake_inc cmake_lib cmake_path
     set all_inc=%loc_inc%;%include%;%loc_dir%\include;
     set all_lib=%loc_lib%;%lib%;%loc_dir%\lib;%loc_dir%\bin;
     set all_bin=%loc_bin%;%path%;%loc_dir%\bin;
-    endlocal & set %~2=%all_inc% & set %~3=%all_lib% & set %~4=%all_bin%
+    endlocal & set %~2=%all_inc% & set %~3=%all_lib% & set %~4=%all_bin% & set %~5=%cmake_inc% & set %~6=%cmake_lib% & set %~7=%cmake_path%
 goto :eof
 
 :ShowProjEnv
@@ -136,6 +143,9 @@ goto :eof
     echo include:%include%
     echo lib:%lib%
     echo path:%path%
+    echo CMAKE_INCLUDE_PATH:%CMAKE_INCLUDE_PATH%
+    echo CMAKE_LIBRARY_PATH:%CMAKE_LIBRARY_PATH%
+    echo CMAKE_MODULE_PATH:%CMAKE_MODULE_PATH%
     call :color_text 9f "--------------ShowProjEnv--------------"
     endlocal
 goto :eof
@@ -189,6 +199,113 @@ goto :eof
     echo .
 goto :eof
 
+
+:get_dir_by_tar
+    setlocal ENABLEDELAYEDEXPANSION
+    set tar_file="%~1"
+    call :color_text 2f "++++++++++++++get_dir_by_tar++++++++++++++"
+    @rem for /f "tokens=8 delims= " %%i in ('tar -tf %tar_file%') do ( echo %%~i )
+    set FileDir=
+    set file_name=
+    echo "    tar -tf %tar_file% | grep "/$" | gawk -F"/" "{ print $1 }" | sed -n "1p"    "
+    FOR /F "usebackq" %%i IN (` tar -tf %tar_file% ^| grep "/$" ^| gawk -F"/" "{print $1}" ^| sed -n "1p" `) DO (set FileDir=%%i)
+    @rem echo tar_file:%tar_file% FileDir:!FileDir!
+    call :is_contain %tar_file% %FileDir% file_name
+    if "%file_name%" == "false" (
+        call :color_text 4f "-------------get_dir_by_tar--------------"
+        echo tar_file:%tar_file% FileDir:%FileDir%
+    )
+    endlocal & set %~2=%FileDir%
+goto :eof
+
+:get_dir_by_zip
+    setlocal ENABLEDELAYEDEXPANSION
+    set zip_file="%~1"
+    call :color_text 2f "++++++++++++++get_dir_by_zip++++++++++++++"
+    @rem for /f "tokens=8 delims= " %%i in ('unzip -v %zip_file%') do ( echo %%~i )
+    set FileDir=
+    set file_name=
+    @rem unzip -v %zip_file% | gawk -F" "  "{ print $8 } " | gawk  -F"/" "{ print $1 }" | sed -n "5p"
+    echo zip_file:%zip_file%
+    FOR /F "usebackq" %%i IN (` unzip -v %zip_file% ^| gawk -F" "  "{ print $8 } " ^| gawk  -F"/" "{ print $1 }" ^| sed -n "5p" `) DO (set FileDir=%%i)
+    FOR /F "usebackq" %%i IN (` unzip -v %zip_file% ^| gawk -F" "  "{print $8}" ^| grep -v "/[^$]" ^| gawk  -F"/" "{ print $1 }" ^| sed -n "5p" `) DO (set FileDir=%%i)
+    @rem echo zip_file:%zip_file% FileDir:!FileDir!
+    call :is_contain %zip_file% %FileDir% file_name
+    if "%file_name%" == "false" (
+        call :color_text 4f "-------------get_dir_by_zip--------------"
+        echo zip_file:%zip_file% FileDir:%FileDir%
+        pause
+    )
+    endlocal & set %~2=%FileDir%
+goto :eof
+
+:gen_env_by_file
+    setlocal ENABLEDELAYEDEXPANSION
+    set zip_file="%~1"
+    set HomeDir=%~2
+    set FileDir=
+
+    call :get_pre_sub_str !zip_file! . file_name
+    call :get_last_char_pos !zip_file! . ext_name_pos
+    echo file_name:!file_name! ext_name_pos:!ext_name_pos!
+    call :get_suf_sub_str !zip_file! . ext_name
+    echo ext_name:!ext_name!
+    if "%ext_name%" == "zip" (
+        call :get_dir_by_zip %zip_file% FileDir
+    ) else if "%ext_name%" == "gz" (
+        call :get_dir_by_tar %zip_file% FileDir
+    ) else if "%ext_name%" == "xz" (
+        call :get_dir_by_tar %zip_file% FileDir
+    ) else (
+        echo "%ext_name%"
+    )
+    call :color_text 9f "++++++++++++++gen_env_by_file++++++++++++++"
+    set DstDirWithHome=%HomeDir%\%FileDir%
+    echo %0 %zip_file% %DstDirWithHome%
+    endlocal & set %~3=%DstDirWithHome%
+goto :eof
+
+:gen_all_env
+    setlocal ENABLEDELAYEDEXPANSION
+    set tools_dir="%~1"
+    set home_dir="%~2"
+    set DstDirWithHome=
+    call :color_text 2f "++++++++++++++gen_all_env++++++++++++++"
+    if not exist %tools_dir% (
+        echo Dir '%tools_dir%' doesn't exist!
+        goto :eof
+    )
+    pushd %tools_dir%
+        for /f %%i in ( 'dir /b *.tar.* *.zip' ) do (
+            set tar_file=%%i
+            call :gen_env_by_file !tar_file! !home_dir! DstDirWithHome
+            set inc=!DstDirWithHome!/include;!inc!
+            set lib=!DstDirWithHome!/lib;!lib!
+            set bin=!DstDirWithHome!/bin;!bin!
+            set CMAKE_INCLUDE_PATH=!DstDirWithHome!/include;!CMAKE_INCLUDE_PATH!
+            set CMAKE_LIBRARY_PATH=!DstDirWithHome!/lib;!CMAKE_LIBRARY_PATH!
+            set CMAKE_MODULE_PATH=!DstDirWithHome!/cmake;!CMAKE_MODULE_PATH!
+        )
+    popd
+    call :color_text 9f "++++++++++++++gen_all_env++++++++++++++"
+    echo inc:%inc%
+    echo lib:%lib%
+    echo bin:%bin%
+    endlocal & set %~3=%inc% & set %~4=%lib% & set %~5=%bin% & set %~6=%CMAKE_INCLUDE_PATH% & set %~7=%CMAKE_LIBRARY_PATH% & set %~8=%CMAKE_MODULE_PATH%
+goto :eof
+
+:show_all_env
+    setlocal ENABLEDELAYEDEXPANSION
+    call :color_text 2f "++++++++++++++show_all_env++++++++++++++"
+    echo all_inc:%all_inc%
+    echo all_lib:%all_lib%
+    echo all_bin:%all_bin%
+    echo CMAKE_INCLUDE_PATH:%CMAKE_INCLUDE_PATH%
+    echo CMAKE_LIBRARY_PATH:%CMAKE_LIBRARY_PATH%
+    echo CMAKE_MODULE_PATH:%CMAKE_MODULE_PATH%
+    endlocal
+goto :eof
+
 :get_str_len
     setlocal ENABLEDELAYEDEXPANSION
     set mystr=%~1
@@ -216,7 +333,7 @@ goto :eof
     call :color_text 2f "++++++++++++++get_first_char_pos++++++++++++++"
     :intercept_first_char_pos
     for /f %%i in ("%count%") do (
-        set /a count+=1	
+        set /a count+=1
         if not "!mystr:~%%i,1!"=="!char_sym!" (
             goto :intercept_first_char_pos
         )
@@ -233,11 +350,11 @@ goto :eof
     call :get_str_len %mystr% mystrlen
     set count=%mystrlen%
     call :color_text 2f "++++++++++++++get_last_char_pos++++++++++++++"
-    @rem set /a count-=1	
+    @rem set /a count-=1
     :intercept_last_char_pos
     for /f %%i in ("%count%") do (
         if not "!mystr:~%%i,1!"=="!char_sym!" (
-            set /a count-=1			
+            set /a count-=1
             goto :intercept_last_char_pos
         )
     )
@@ -289,10 +406,23 @@ goto :eof
         if not "!mystr:~%%i,1!"=="!char_sym!" (
             set /a mysubstr_len=!mystrlen! - %%i
             set substr=!mystr:~%%i!
-            set /a count-=1	
+            set /a count-=1
             goto :intercept_suf_sub_str
         )
     )
     echo %0 %mystr% %char_sym% %count% %mysubstr_len%
     endlocal & set %~3=%substr%
+goto :eof
+
+:GetCurSysTime
+    setlocal EnableDelayedExpansion
+    set dateStr=
+    set timeStr=
+    set year=%date:~6,4%
+    set month=%date:~4,2%
+    set day=%date:~0,2%
+    set dateStr=%year%_%month%_%day%
+    set dateStr=%dateStr:/=%
+    set timeStr=%time::=%
+    endlocal & set %~1=%dateStr%_%timeStr%
 goto :eof
